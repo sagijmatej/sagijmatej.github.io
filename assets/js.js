8c3b788ccdaec4e5f4929833f86be82ffefa8b1e
sagijmatej <matej.sagij@gmail.com>
2025-04-15 15:26:30 +0200

    Add files via upload

--- assets/js.js
+++ assets/js.js
@@ -1,7 +1,7 @@
 
 //Splide 
 document.addEventListener('DOMContentLoaded', function () {
-    var splide = new Splide( '#image-carousel', {
+    var splide = new Splide( '#image-carousel-front', {
         type  : 'fade',
         rewind: true,
         pagination: false,
