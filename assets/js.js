9eae57905dfc0bf3626be2a8edc99470a8071ad8
sagijmatej <matej.sagij@gmail.com>
2025-04-17 15:20:06 +0200

    Add files via upload

--- assets/js.js
+++ assets/js.js
@@ -1,11 +1,37 @@
 
 //Splide 
 document.addEventListener('DOMContentLoaded', function () {
-    var splide = new Splide( '#image-carousel-front', {
-        type  : 'fade',
-        rewind: true,
-        pagination: false,
-      } );
-      
-      splide.mount();
+  var splide = new Splide('#image-carousel-front', {
+    type: 'fade',
+    rewind: true,
+    pagination: false,
+  });
+
+  splide.mount();
 });
+
+
+
+
+//Work page
+$('.button-f3').click(function () {
+  window.location.href = 'about.html';
+});
+
+$(".project-wrapper").hover(
+  function () {
+    $(this).find(".project-info").show()
+  },
+  function () {
+    $(this).find(".project-info").hide()
+  },
+);
+
+$(".project-wrapper").hover(
+  function () {
+    $(this).find(".project-photo-mask").show()
+  },
+  function () {
+    $(this).find(".project-photo-mask").hide()
+  },
+);
